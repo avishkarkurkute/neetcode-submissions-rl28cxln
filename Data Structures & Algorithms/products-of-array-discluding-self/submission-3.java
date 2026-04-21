a0365e3b626ce93a02bdebad87cae0bc95592060
Avishkar Kurkute <148682828+avishkarkurkute@users.noreply.github.com>
2026-04-21 20:19:21 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int num = 1;
+        int count = 0;
+        int index = 0;
+        boolean isZeroExist = false;
+        for(int i = 0;i < nums.length;i++){
+            num *= nums[i];
+            if(nums[i] == 0){
+                isZeroExist = true;
+            }
+        }
+        if(isZeroExist){
+            num = 1;
+            for(int i = 0;i < nums.length;i++){
+                if(nums[i] == 0){
+                    count++;
+                    index = i;
+                    continue;
+                }
+                num *= nums[i];
+            }
+            if(count > 1){
+                return new int[nums.length];
+            }
+            if(count == 1){
+                int[] exSelfProductP = new int[nums.length];
+                exSelfProductP[index] = num;
+                return exSelfProductP;
+            }
+        }
+        int[] exSelfProduct = new int[nums.length];
+
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i] != 0){
+                exSelfProduct[i] = num/nums[i];
+            }
+        }
+        return exSelfProduct;
+    }
+}  
